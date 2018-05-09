ddfbc4e895e2c912fdafc57f1fddec254d25c056
ambuitragoar05 <ambuitragoar@unal.edu.co>
2018-05-09 10:16:12 -0500

    Precios Primera semana de mayo
    Boletin

--- resources/qgis2web.js
+++ resources/qgis2web.js
@@ -32,7 +32,7 @@ var map = new ol.Map({
 var layerSwitcher = new ol.control.LayerSwitcher({tipLabel: "Layers"});
 map.addControl(layerSwitcher);
 
-map.getView().fit([-9152457.585542, -501794.379083, -6683503.851413, 1408299.414834], map.getSize());
+map.getView().fit([-9128852.231980, -578222.667557, -6663062.134887, 1331010.904357], map.getSize());
 
 var NO_POPUP = 0
 var ALL_FIELDS = 1
