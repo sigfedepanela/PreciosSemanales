c46180a4dca581a86d2adee30f63a0ee79cb3fc2
sigfedepanela <fedepanela2018>
2019-01-23 15:52:38 -0500

    2 boletin de precios 2019
    segunda semana

--- layers/layers.js
+++ layers/layers.js
@@ -48,33 +48,33 @@ jsonSource_MunicipiosTomadePrecios_3.addFeatures(features_MunicipiosTomadePrecio
                 source:jsonSource_MunicipiosTomadePrecios_3, 
                 style: style_MunicipiosTomadePrecios_3,
                 title: '<img src="styles/legend/MunicipiosTomadePrecios_3.png" /> Municipios Toma de Precios'
-            });var format_PreciosSegundaSemanadeDiciembrede2018_4 = new ol.format.GeoJSON();
-var features_PreciosSegundaSemanadeDiciembrede2018_4 = format_PreciosSegundaSemanadeDiciembrede2018_4.readFeatures(json_PreciosSegundaSemanadeDiciembrede2018_4, 
+            });var format_PreciosSegundaSemanadeEnerode2019_4 = new ol.format.GeoJSON();
+var features_PreciosSegundaSemanadeEnerode2019_4 = format_PreciosSegundaSemanadeEnerode2019_4.readFeatures(json_PreciosSegundaSemanadeEnerode2019_4, 
             {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
-var jsonSource_PreciosSegundaSemanadeDiciembrede2018_4 = new ol.source.Vector({
+var jsonSource_PreciosSegundaSemanadeEnerode2019_4 = new ol.source.Vector({
     attributions: '<a href=""></a>',
 });
-jsonSource_PreciosSegundaSemanadeDiciembrede2018_4.addFeatures(features_PreciosSegundaSemanadeDiciembrede2018_4);var lyr_PreciosSegundaSemanadeDiciembrede2018_4 = new ol.layer.Vector({
+jsonSource_PreciosSegundaSemanadeEnerode2019_4.addFeatures(features_PreciosSegundaSemanadeEnerode2019_4);var lyr_PreciosSegundaSemanadeEnerode2019_4 = new ol.layer.Vector({
                 declutter: true,
-                source:jsonSource_PreciosSegundaSemanadeDiciembrede2018_4, 
-                style: style_PreciosSegundaSemanadeDiciembrede2018_4,
-                title: '<img src="styles/legend/PreciosSegundaSemanadeDiciembrede2018_4.png" /> Precios Segunda Semana de Diciembre de 2018'
+                source:jsonSource_PreciosSegundaSemanadeEnerode2019_4, 
+                style: style_PreciosSegundaSemanadeEnerode2019_4,
+                title: '<img src="styles/legend/PreciosSegundaSemanadeEnerode2019_4.png" /> Precios Segunda Semana de Enero de 2019'
             });
 
-lyr_OpenStreetMap_0.setVisible(true);lyr_LimiteDepartamental_1.setVisible(true);lyr_PreciosporDepartamento_2.setVisible(true);lyr_MunicipiosTomadePrecios_3.setVisible(true);lyr_PreciosSegundaSemanadeDiciembrede2018_4.setVisible(false);
-var layersList = [lyr_OpenStreetMap_0,lyr_LimiteDepartamental_1,lyr_PreciosporDepartamento_2,lyr_MunicipiosTomadePrecios_3,lyr_PreciosSegundaSemanadeDiciembrede2018_4];
+lyr_OpenStreetMap_0.setVisible(true);lyr_LimiteDepartamental_1.setVisible(true);lyr_PreciosporDepartamento_2.setVisible(true);lyr_MunicipiosTomadePrecios_3.setVisible(true);lyr_PreciosSegundaSemanadeEnerode2019_4.setVisible(false);
+var layersList = [lyr_OpenStreetMap_0,lyr_LimiteDepartamental_1,lyr_PreciosporDepartamento_2,lyr_MunicipiosTomadePrecios_3,lyr_PreciosSegundaSemanadeEnerode2019_4];
 lyr_LimiteDepartamental_1.set('fieldAliases', {'DEPT': 'DEPT', });
 lyr_PreciosporDepartamento_2.set('fieldAliases', {'Departamen': 'Departamen', 'Año': 'Año', 'MES': 'MES', 'SEMANA': 'SEMANA', '$ por Kg': '$ por Kg', '$ Prom Año': '$ Prom Año', 'Prom_ Pais': 'Prom_ Pais', });
 lyr_MunicipiosTomadePrecios_3.set('fieldAliases', {'DEPT': 'DEPT', 'MUNICIPIO': 'MUNICIPIO', });
-lyr_PreciosSegundaSemanadeDiciembrede2018_4.set('fieldAliases', {'DEPT': 'DEPT', 'MUNICIPIO': 'MUNICIPIO', });
+lyr_PreciosSegundaSemanadeEnerode2019_4.set('fieldAliases', {'DEPT': 'DEPT', 'MUNICIPIO': 'MUNICIPIO', });
 lyr_LimiteDepartamental_1.set('fieldImages', {'DEPT': 'TextEdit', });
 lyr_PreciosporDepartamento_2.set('fieldImages', {'Departamen': 'TextEdit', 'Año': 'TextEdit', 'MES': 'TextEdit', 'SEMANA': 'TextEdit', '$ por Kg': 'TextEdit', '$ Prom Año': 'TextEdit', 'Prom_ Pais': 'TextEdit', });
 lyr_MunicipiosTomadePrecios_3.set('fieldImages', {'DEPT': 'TextEdit', 'MUNICIPIO': 'TextEdit', });
-lyr_PreciosSegundaSemanadeDiciembrede2018_4.set('fieldImages', {'DEPT': 'TextEdit', 'MUNICIPIO': 'TextEdit', });
+lyr_PreciosSegundaSemanadeEnerode2019_4.set('fieldImages', {'DEPT': 'TextEdit', 'MUNICIPIO': 'TextEdit', });
 lyr_LimiteDepartamental_1.set('fieldLabels', {'DEPT': 'inline label', });
 lyr_PreciosporDepartamento_2.set('fieldLabels', {'Departamen': 'inline label', 'Año': 'inline label', 'MES': 'inline label', 'SEMANA': 'inline label', '$ por Kg': 'inline label', '$ Prom Año': 'inline label', 'Prom_ Pais': 'inline label', });
 lyr_MunicipiosTomadePrecios_3.set('fieldLabels', {'DEPT': 'inline label', 'MUNICIPIO': 'inline label', });
-lyr_PreciosSegundaSemanadeDiciembrede2018_4.set('fieldLabels', {'DEPT': 'inline label', 'MUNICIPIO': 'inline label', });
-lyr_PreciosSegundaSemanadeDiciembrede2018_4.on('precompose', function(evt) {
+lyr_PreciosSegundaSemanadeEnerode2019_4.set('fieldLabels', {'DEPT': 'inline label', 'MUNICIPIO': 'inline label', });
+lyr_PreciosSegundaSemanadeEnerode2019_4.on('precompose', function(evt) {
     evt.context.globalCompositeOperation = 'normal';
 });
