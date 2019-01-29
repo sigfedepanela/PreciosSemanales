d9ebba6e4c440da0c1439818ea109111fce45a23
sigfedepanela <fedepanela2018>
2019-01-29 15:29:33 -0500

    3 semana de enero
    Boletín 3 semana de enero.

--- resources/qgis2web.js
+++ resources/qgis2web.js
@@ -32,7 +32,7 @@ var map = new ol.Map({
 var layerSwitcher = new ol.control.LayerSwitcher({tipLabel: "Layers"});
 map.addControl(layerSwitcher);
 
-map.getView().fit([-9870213.288365, -633647.316831, -6218444.036070, 1542276.741675], map.getSize());
+map.getView().fit([-9602114.879267, -633652.197600, -6481666.628243, 1542289.001321], map.getSize());
 
 var NO_POPUP = 0
 var ALL_FIELDS = 1
