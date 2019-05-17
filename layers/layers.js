be447956cd86ab2299ea7cd1049cd92c62cbc98f
sigfedepanela <fedepanela2018>
2019-05-17 16:24:16 -0500

    2 semana de mayo
    Panelas corrientes

--- layers/layers.js
+++ layers/layers.js
@@ -32,11 +32,11 @@ jsonSource_PreciosporDepartamento_2.addFeatures(features_PreciosporDepartamento_
                 source:jsonSource_PreciosporDepartamento_2, 
                 style: style_PreciosporDepartamento_2,
     title: 'Precios por Departamento<br />\
-    <img src="styles/legend/PreciosporDepartamento_2_0.png" />  1127 - 1138 <br />\
-    <img src="styles/legend/PreciosporDepartamento_2_1.png" />  1138 - 1419 <br />\
-    <img src="styles/legend/PreciosporDepartamento_2_2.png" />  1419 - 1597 <br />\
-    <img src="styles/legend/PreciosporDepartamento_2_3.png" />  1597 - 1837 <br />\
-    <img src="styles/legend/PreciosporDepartamento_2_4.png" />  1837 - 2228 <br />'
+    <img src="styles/legend/PreciosporDepartamento_2_0.png" />  1475 - 1475 <br />\
+    <img src="styles/legend/PreciosporDepartamento_2_1.png" />  1475 - 1575 <br />\
+    <img src="styles/legend/PreciosporDepartamento_2_2.png" />  1575 - 1638 <br />\
+    <img src="styles/legend/PreciosporDepartamento_2_3.png" />  1638 - 1793 <br />\
+    <img src="styles/legend/PreciosporDepartamento_2_4.png" />  1793 - 2140 <br />'
         });var format_MunicipiosTomadePrecios_3 = new ol.format.GeoJSON();
 var features_MunicipiosTomadePrecios_3 = format_MunicipiosTomadePrecios_3.readFeatures(json_MunicipiosTomadePrecios_3, 
             {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
@@ -48,33 +48,19 @@ jsonSource_MunicipiosTomadePrecios_3.addFeatures(features_MunicipiosTomadePrecio
                 source:jsonSource_MunicipiosTomadePrecios_3, 
                 style: style_MunicipiosTomadePrecios_3,
                 title: '<img src="styles/legend/MunicipiosTomadePrecios_3.png" /> Municipios Toma de Precios'
-            });var format_PreciosTerceraSemanadeAbrilde2019_4 = new ol.format.GeoJSON();
-var features_PreciosTerceraSemanadeAbrilde2019_4 = format_PreciosTerceraSemanadeAbrilde2019_4.readFeatures(json_PreciosTerceraSemanadeAbrilde2019_4, 
-            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
-var jsonSource_PreciosTerceraSemanadeAbrilde2019_4 = new ol.source.Vector({
-    attributions: '<a href=""></a>',
-});
-jsonSource_PreciosTerceraSemanadeAbrilde2019_4.addFeatures(features_PreciosTerceraSemanadeAbrilde2019_4);var lyr_PreciosTerceraSemanadeAbrilde2019_4 = new ol.layer.Vector({
-                declutter: true,
-                source:jsonSource_PreciosTerceraSemanadeAbrilde2019_4, 
-                style: style_PreciosTerceraSemanadeAbrilde2019_4,
-                title: '<img src="styles/legend/PreciosTerceraSemanadeAbrilde2019_4.png" /> Precios Tercera  Semana de Abril de 2019'
             });
 
-lyr_OpenStreetMap_0.setVisible(true);lyr_LimiteDepartamental_1.setVisible(true);lyr_PreciosporDepartamento_2.setVisible(true);lyr_MunicipiosTomadePrecios_3.setVisible(true);lyr_PreciosTerceraSemanadeAbrilde2019_4.setVisible(false);
-var layersList = [lyr_OpenStreetMap_0,lyr_LimiteDepartamental_1,lyr_PreciosporDepartamento_2,lyr_MunicipiosTomadePrecios_3,lyr_PreciosTerceraSemanadeAbrilde2019_4];
+lyr_OpenStreetMap_0.setVisible(true);lyr_LimiteDepartamental_1.setVisible(true);lyr_PreciosporDepartamento_2.setVisible(true);lyr_MunicipiosTomadePrecios_3.setVisible(true);
+var layersList = [lyr_OpenStreetMap_0,lyr_LimiteDepartamental_1,lyr_PreciosporDepartamento_2,lyr_MunicipiosTomadePrecios_3];
 lyr_LimiteDepartamental_1.set('fieldAliases', {'DEPT': 'DEPT', });
 lyr_PreciosporDepartamento_2.set('fieldAliases', {'Departamen': 'Departamen', 'Año': 'Año', 'MES': 'MES', 'SEMANA': 'SEMANA', '$ por Kg': '$ por Kg', '$ Prom Año': '$ Prom Año', 'Prom_ Pais': 'Prom_ Pais', });
 lyr_MunicipiosTomadePrecios_3.set('fieldAliases', {'DEPT': 'DEPT', 'MUNICIPIO': 'MUNICIPIO', });
-lyr_PreciosTerceraSemanadeAbrilde2019_4.set('fieldAliases', {'DEPT': 'DEPT', 'MUNICIPIO': 'MUNICIPIO', });
 lyr_LimiteDepartamental_1.set('fieldImages', {'DEPT': 'TextEdit', });
 lyr_PreciosporDepartamento_2.set('fieldImages', {'Departamen': 'TextEdit', 'Año': 'TextEdit', 'MES': 'TextEdit', 'SEMANA': 'TextEdit', '$ por Kg': 'TextEdit', '$ Prom Año': 'TextEdit', 'Prom_ Pais': 'TextEdit', });
 lyr_MunicipiosTomadePrecios_3.set('fieldImages', {'DEPT': 'TextEdit', 'MUNICIPIO': 'TextEdit', });
-lyr_PreciosTerceraSemanadeAbrilde2019_4.set('fieldImages', {'DEPT': 'TextEdit', 'MUNICIPIO': 'TextEdit', });
-lyr_LimiteDepartamental_1.set('fieldLabels', {'DEPT': 'inline label', });
+lyr_LimiteDepartamental_1.set('fieldLabels', {'DEPT': 'no label', });
 lyr_PreciosporDepartamento_2.set('fieldLabels', {'Departamen': 'inline label', 'Año': 'inline label', 'MES': 'inline label', 'SEMANA': 'inline label', '$ por Kg': 'inline label', '$ Prom Año': 'inline label', 'Prom_ Pais': 'inline label', });
 lyr_MunicipiosTomadePrecios_3.set('fieldLabels', {'DEPT': 'inline label', 'MUNICIPIO': 'inline label', });
-lyr_PreciosTerceraSemanadeAbrilde2019_4.set('fieldLabels', {'DEPT': 'inline label', 'MUNICIPIO': 'inline label', });
-lyr_PreciosTerceraSemanadeAbrilde2019_4.on('precompose', function(evt) {
+lyr_MunicipiosTomadePrecios_3.on('precompose', function(evt) {
     evt.context.globalCompositeOperation = 'normal';
 });
--- styles/LimiteDepartamental_1_style.js
+++ styles/LimiteDepartamental_1_style.js
@@ -21,7 +21,7 @@ var style_LimiteDepartamental_1 = function(feature, resolution){
         labelText = String("");
     }
     var style = [ new ol.style.Style({
-        stroke: new ol.style.Stroke({color: 'rgba(128,152,72,0.45)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}),fill: new ol.style.Fill({color: 'rgba(186,221,105,0.45)'}),
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}),fill: new ol.style.Fill({color: 'rgba(190,207,80,0.0)'}),
         text: createTextStyle(feature, resolution, labelText, labelFont,
                               labelFill, placement, bufferColor,
                               bufferWidth)
